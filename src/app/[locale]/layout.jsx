fa6424b6cc6ea95825254f64e70454d689c33cce
Ahmed-Mohammed-Badawi <AhmedIsmail73990@azhar.edu.eg>
2024-11-19 21:30:26 +0200

    ADD THE FACEBOOK META TAG

--- src/app/[locale]/layout.jsx
+++ src/app/[locale]/layout.jsx
@@ -1,38 +1,41 @@
-import { NextIntlClientProvider } from "next-intl";
-import { getMessages } from "next-intl/server";
-import { Toaster } from 'react-hot-toast';
+import {NextIntlClientProvider} from "next-intl";
+import {getMessages} from "next-intl/server";
+import {Toaster} from 'react-hot-toast';
 
 
 import "./../globals.css";
 
-export async function generateMetadata({ params: { locale } }) {
-	return {
-		title: locale === "en" ? "Fixawi" : "فيكساوي",
-		description:
-			locale === "en"
-				? "Fixawi is a private company focused on simplifying car maintenance for owners. We ensure vehicles are kept in excellent condition, offering convenient and flexible services in terms of time and location at competitive prices."
-				: "فيكساوي هي شركة خاصة تركز على تبسيط صيانة السيارات لأصحابها. نحن نضمن أن تبقى السيارات في حالة ممتازة، ونقدم خدمات مريحة ومرنة من حيث الوقت والموقع بأسعار تنافسية.",
-		locale,
-		siteName: locale === "en" ? "Fixawi" : "فيكساوي",
-		icon: "/favicon.ico",
-	};
+export async function generateMetadata({params: {locale}}) {
+    return {
+        title: locale === "en" ? "Fixawi" : "فيكساوي",
+        description:
+            locale === "en"
+                ? "Fixawi is a private company focused on simplifying car maintenance for owners. We ensure vehicles are kept in excellent condition, offering convenient and flexible services in terms of time and location at competitive prices."
+                : "فيكساوي هي شركة خاصة تركز على تبسيط صيانة السيارات لأصحابها. نحن نضمن أن تبقى السيارات في حالة ممتازة، ونقدم خدمات مريحة ومرنة من حيث الوقت والموقع بأسعار تنافسية.",
+        locale,
+        siteName: locale === "en" ? "Fixawi" : "فيكساوي",
+        icon: "/favicon.ico",
+    };
 }
 
-export default async function LocaleLayout({ children, params: { locale } }) {
-	// Providing all messages to the client
-	// side is the easiest way to get started
-	const messages = await getMessages();
+export default async function LocaleLayout({children, params: {locale}}) {
+    // Providing all messages to the client
+    // side is the easiest way to get started
+    const messages = await getMessages();
 
-	return (
-		<html lang={locale} dir={locale === "ar" ? "rtl" : "ltr"}>
-			<body>
-				<NextIntlClientProvider messages={messages}>
-					{children}
-					<Toaster
-						position={"bottom-right"}
-					/>
-				</NextIntlClientProvider>
-			</body>
-		</html>
-	);
+    return (
+        <html lang={locale} dir={locale === "ar" ? "rtl" : "ltr"}>
+        <head>
+            <meta name="facebook-domain-verification" content="q2s610s795eshep0z0h2lv3foxj86t"/>
+        </head>
+        <body>
+        <NextIntlClientProvider messages={messages}>
+            {children}
+            <Toaster
+                position={"bottom-right"}
+            />
+        </NextIntlClientProvider>
+        </body>
+        </html>
+    );
 }
