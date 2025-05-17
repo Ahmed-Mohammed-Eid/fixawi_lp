0f8ad32c4aacb515d6267fd822717b5d83d0e991
Ahmed Mohammed <AhmedIsmail73990@azhar.edu.eg>
2025-05-17 13:32:23 +0300

    update Hero component layout and add external links for app downloads

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -13,76 +13,86 @@ import heroBackground from "../assets/hero_background_copy.png";
 import classes from "./Hero.module.css";
 
 export default function Hero() {
-	const t = useTranslations();
+    const t = useTranslations();
 
-	return (
-		<header
-			id="Hero"
-			className={`${classes.Hero} flex justify-start flex-col items-start`}
-		>
-			<div className="fx-container mx-auto">
-				<Image
-					src={heroBackground}
-					alt="Hero background"
-					className={classes.Hero_background}
-					priority
-					fill
-				/>
-				<Link href="/">
-					<Image
-						src={logo}
-						alt="logo"
-						width={350}
-						height={200}
-						className={
-							"block md:mb-[80px] md:mt-[163px] mt-[100px] mb-[40px] " +
-							classes.Hero_logo
-						}
-					/>
-				</Link>
+    return (
+        <header
+            id="Hero"
+            className={`${classes.Hero} flex justify-start flex-col items-start`}
+        >
+            <div className="fx-container mx-auto">
+                <Image
+                    src={heroBackground}
+                    alt="Hero background"
+                    className={classes.Hero_background}
+                    priority
+                    fill
+                />
+                <Link href="/">
+                    <Image
+                        src={logo}
+                        alt="logo"
+                        width={350}
+                        height={200}
+                        className={
+                            "block md:mb-[80px] md:mt-[163px] mt-[100px] mb-[40px] " +
+                            classes.Hero_logo
+                        }
+                    />
+                </Link>
 
-				<p className={classes.Hero_paragraph}>
-					<span className="text-bold text-[22px] lg:text-[34px]">
-						{t("Hero.heroTextBold")}
-					</span>
-					{t("Hero.heroText")}
-				</p>
+                <p className={classes.Hero_paragraph}>
+                    <span className="text-bold text-[22px] lg:text-[34px]">
+                        {t("Hero.heroTextBold")}
+                    </span>
+                    {t("Hero.heroText")}
+                </p>
 
-				<div className={classes.Store_buttons}>
-					<Link href="/download" className="btn">
-						<Image
-							src={playStoreImage}
-							alt="play store"
-							width={250}
-							height={73.57}
-						/>
-					</Link>
-					<div className={"w-[73.57px] bg-white " + classes.qr}>
-						<Image
-							src={appStoreQR}
-							alt="app store"
-							width={73.57}
-							height={73.57}
-						/>
-					</div>
-					<Link href="/download" className="btn">
-						<Image
-							src={appStoreImage}
-							alt="app store"
-							width={250}
-							height={73.57}
-						/>
-					</Link>
-					<div className={"w-[73.57px] bg-white " + classes.qr}>
-						<Image
-							src={playStoreQR}
-							alt="play store"
-							width={73.57}
-							height={73.57}
-						/>
-					</div>
-				</div>
-			</div>
-		</header>
-	);
+                <div className={classes.Store_buttons}>
+                    {/* GOOGLE */}
+                    <Link
+                        href="https://play.google.com/store/apps/details?id=com.nxtLeap.Fixawi"
+                        className="btn"
+                        target="_blank"
+                    >
+                        <Image
+                            src={playStoreImage}
+                            alt="play store"
+                            width={250}
+                            height={73.57}
+                        />
+                    </Link>
+                    <div className={"w-[73.57px] bg-white " + classes.qr}>
+                        <Image
+                            src={playStoreQR}
+                            alt="play store"
+                            width={73.57}
+                            height={73.57}
+                        />
+                    </div>
+                    {/* APPLE */}
+                    <Link
+                        href="https://play.google.com/store/apps/details?id=com.nxtLeap.Fixawi"
+                        className="btn"
+                        target="_blank"
+                    >
+                        <Image
+                            src={appStoreImage}
+                            alt="app store"
+                            width={250}
+                            height={73.57}
+                        />
+                    </Link>
+                    <div className={"w-[73.57px] bg-white " + classes.qr}>
+                        <Image
+                            src={appStoreQR}
+                            alt="app store"
+                            width={73.57}
+                            height={73.57}
+                        />
+                    </div>
+                </div>
+            </div>
+        </header>
+    );
 }
